9456de824296a552f50f94f3cd067954f4dd1758
Baptiste Caquot <baptistecaquot@gmail.com>
2017-05-07 23:41:13 +0200

    change config

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Adopteunpanneau2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.configParti = config_for(:configParti)
   end
 end
